ebabd7c9717e2341d25219a9545405aeb20c5d92
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-06-28 12:39:04 +0200

    Check bomb spawn position

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -18,8 +18,20 @@ public class GameManager : MonoBehaviour{
             Transform posPlayer = GameObject.Find("Player(Clone)").transform;
             float posX = (float)Math.Round(posPlayer.position.x);
             float posZ = (float)Math.Round(posPlayer.position.z);
-            GameObject clone = (GameObject) Instantiate(bomb, new Vector3 (posX, 1, posZ), Quaternion.identity);
-            Destroy(clone, 1.5f);
+
+            GameObject[] piedras = GameObject.FindGameObjectsWithTag("Piedra");
+            bool isRock = false;
+
+            for(int x = 0; x < piedras.Length && !isRock; x++){
+                if(piedras[x].transform.position.x == posX && piedras[x].transform.position.z == posZ){
+                    isRock = true;
+                }
+            }
+
+            if(!isRock){
+                GameObject clone = (GameObject) Instantiate(bomb, new Vector3 (posX, 1, posZ), Quaternion.identity);
+                Destroy(clone, 1.5f);
+            }
         }
         if(Input.GetKeyDown("p")){
             Debug.Log("P pressed");            
