1a0b802391744818c67624b580ffd9244ac27a5e
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-06-29 13:59:54 +0200

    Explosion kill player

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,51 +3,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour{
     public GameObject bomb;
     public Canvas pause;
     float posX = 0;
     float posZ = 0;
+    bool dead = false;
 
     void Start() {
         pause.gameObject.SetActive(false);
     }
 
     void Update(){
-        if(Input.GetKeyDown("space")){
-            Transform posPlayer = GameObject.Find("Player(Clone)").transform;
-            posX = (float)Math.Round(posPlayer.position.x);
-            posZ = (float)Math.Round(posPlayer.position.z);
-            GameObject[] rocks = GameObject.FindGameObjectsWithTag("Rock");
-            GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
-            bool occuped = false;
-            
-            if(rocks != null){
-                for(int x = 0; x < rocks.Length && !occuped; x++){
-                    if(rocks[x].transform.position.x == posX && rocks[x].transform.position.z == posZ){
-                        occuped = true;
+        if(!dead){
+            if(Input.GetKeyDown("space")){
+                Transform posPlayer = GameObject.Find("Player(Clone)").transform;
+                posX = (float)Math.Round(posPlayer.position.x);
+                posZ = (float)Math.Round(posPlayer.position.z);
+                GameObject[] rocks = GameObject.FindGameObjectsWithTag("Rock");
+                GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
+                bool occuped = false;
+                
+                if(rocks != null){
+                    for(int x = 0; x < rocks.Length && !occuped; x++){
+                        if(rocks[x].transform.position.x == posX && rocks[x].transform.position.z == posZ){
+                            occuped = true;
+                        }
                     }
                 }
-            }
-            
-            if(bombs != null){
-                for(int x = 0; x < bombs.Length && !occuped; x++){
-                    if(bombs[x].transform.position.x == posX && bombs[x].transform.position.z == posZ){
-                        occuped = true;
+                
+                if(bombs != null){
+                    for(int x = 0; x < bombs.Length && !occuped; x++){
+                        if(bombs[x].transform.position.x == posX && bombs[x].transform.position.z == posZ){
+                            occuped = true;
+                        }
                     }
+                }
+
+                if(!occuped){
+                    GameObject clone = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ), new Quaternion(0, 180, 0, 1));
+                    Invoke("explode", 1.5f);
+                    Destroy(clone, 1.5f);
                 }
             }
 
-            if(!occuped){
-                GameObject clone = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ), new Quaternion(0, 180, 0, 1));
-                Invoke("explode", 1.5f);
-                Destroy(clone, 1.5f);
+            if(Input.GetKeyDown("p")){
+                Pause();
             }
-        }
-
-        if(Input.GetKeyDown("p")){
-            Pause();
+        }else{
+            SceneManager.LoadScene("InitialScene", LoadSceneMode.Single);
         }
     }
 
@@ -86,22 +92,27 @@ public class GameManager : MonoBehaviour{
 
         if(up){
             GameObject exp = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ1), new Quaternion(0, 180, 0, 1));
+            checkExplosion(posX, posZ1);
             Destroy(exp, 0.5f);
         }
         if(down){
             GameObject exp = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ2), new Quaternion(0, 180, 0, 1));
+            checkExplosion(posX, posZ2);
             Destroy(exp, 0.5f);
         }
         if(left){
             GameObject exp = (GameObject) Instantiate(bomb, new Vector3(posX2, 1, posZ), new Quaternion(0, 180, 0, 1));
+            checkExplosion(posX2, posZ);
             Destroy(exp, 0.5f);
         }
         if(right){
             GameObject exp = (GameObject) Instantiate(bomb, new Vector3(posX1, 1, posZ), new Quaternion(0, 180, 0, 1));
+            checkExplosion(posX1, posZ);
             Destroy(exp, 0.5f);
         }
 
         GameObject exp2 = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ), new Quaternion(0, 180, 0, 1));
+        checkExplosion(posX, posZ);
         Destroy(exp2, 0.5f);
     }
 
@@ -113,6 +124,16 @@ public class GameManager : MonoBehaviour{
         }else if(Time.timeScale == 0){
             Time.timeScale = 1;
             pause.gameObject.SetActive(false);
+        }
+    }
+
+    void checkExplosion(float X, float Z){
+        Transform posPlayer = GameObject.Find("Player(Clone)").transform;
+        float playerX = (float)Math.Round(posPlayer.position.x);
+        float playerZ = (float)Math.Round(posPlayer.position.z);
+
+        if(playerX == X && playerZ == Z){
+            dead = true;
         }
     }
 }
