c8b421e0f486371dece16f242891cbcc05565a5b
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-07-05 12:58:17 +0200

    Random bombs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,14 +5,17 @@ using UnityEngine.AI;
 using System;
 using UnityEngine.SceneManagement;
 using System.Linq;
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour{
     public GameObject bomb;
     public GameObject fire;
     public Canvas pause;
     public float timer = 60.0f;
-    float posX = 0;
-    float posZ = 0;
+    float bombX = 0;
+    float bombZ = 0;
+    int randomBombX = 0;
+    int randomBombZ = 0;
     int distance = 1;
     bool dead = false;
     bool play = true;
@@ -20,6 +23,7 @@ public class GameManager : MonoBehaviour{
     public int score = 0;
     List<GameObject> boxs = new List<GameObject>();
     public List<GameObject> powerUps = new List<GameObject>();
+    bool random = false;
     void Start() {
         timer = 60.0f;
         pause.gameObject.SetActive(false);
@@ -42,28 +46,29 @@ public class GameManager : MonoBehaviour{
             if(!dead){
                 if(Input.GetKeyDown("space")){
                     Transform posPlayer = GameObject.Find("Player(Clone)").transform;
-                    posX = (float)Math.Round(posPlayer.position.x);
-                    posZ = (float)Math.Round(posPlayer.position.z);
+                    bombX = (float)Math.Round(posPlayer.position.x);
+                    bombZ = (float)Math.Round(posPlayer.position.z);
                     GameObject[] bombs = GameObject.FindGameObjectsWithTag("Bomb");
                     bool occuped = false;
                     //No pongo 2 bombas en el mismo sitio
                     if(bombs != null){
                         for(int x = 0; x < bombs.Length && !occuped; x++){
-                            if(bombs[x].transform.position.x == posX && bombs[x].transform.position.z == posZ){
+                            if(bombs[x].transform.position.x == bombX && bombs[x].transform.position.z == bombZ){
                                 occuped = true;
                             }
                         }
                     }
 
                     if(!occuped){
-                        GameObject clone = (GameObject) Instantiate(bomb, new Vector3(posX, 1, posZ), new Quaternion(0, 180, 0, 1));
+                        GameObject clone = (GameObject) Instantiate(bomb, new Vector3(bombX, 1, bombZ), new Quaternion(0, 180, 0, 1));
                         Invoke("explode", 1.5f);
                         Destroy(clone, 1.5f);
                     }
                 }
-                boxs = GameObject.FindGameObjectsWithTag("Box").ToList();
                 UpdatePowerUps();
-                RandomBomb();
+                if(!random){
+                    RandomBomb();
+                }
                 if(Input.GetKeyDown("p")){
                     Pause();
                 }
@@ -74,6 +79,7 @@ public class GameManager : MonoBehaviour{
     }
 
     void explode(){
+        boxs = GameObject.FindGameObjectsWithTag("Box").ToList();
         int aux = 1;
         bool up    = true;
         bool down  = true;
@@ -85,15 +91,15 @@ public class GameManager : MonoBehaviour{
         }
 
         while(aux != distance + 1){
-            float posZ1 = posZ + aux;
-            float posZ2 = posZ - aux;
-            float posX1 = posX + aux;
-            float posX2 = posX - aux;
+            float bombZ1 = bombZ + aux;
+            float bombZ2 = bombZ - aux;
+            float bombX1 = bombX + aux;
+            float bombX2 = bombX - aux;
 
             if(boxs != null){
                 for(int x = 0; x < boxs.Count; x++){
                     //UP
-                    if(up && boxs[x].transform.position.x == posX && boxs[x].transform.position.z == posZ1){ //Puedo mirar arriba y hay algo ahí
+                    if(up && boxs[x].transform.position.x == bombX && boxs[x].transform.position.z == bombZ1){ //Puedo mirar arriba y hay algo ahí
                         if(boxs[x].TryGetComponent(out Wall rock)){ //Si es roca dejo de mirar
                             up = false;
                         }else{ //Si no, es caja, la destruyo
@@ -103,12 +109,12 @@ public class GameManager : MonoBehaviour{
                         
                     }
                     if(up){ //Si arriba no hay roca, creo la explosion y la compruebo
-                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(posX, 1.5f, posZ1), new Quaternion(0, 180, 0, 1));
-                        checkExplosion(posX, posZ1);
+                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(bombX, 1.5f, bombZ1), new Quaternion(0, 180, 0, 1));
+                        checkExplosion(bombX, bombZ1);
                         Destroy(exp, 0.5f);
                     }
                     //DOWN
-                    if(down && boxs[x].transform.position.x == posX && boxs[x].transform.position.z == posZ2){
+                    if(down && boxs[x].transform.position.x == bombX && boxs[x].transform.position.z == bombZ2){
                         if(boxs[x].TryGetComponent(out Wall rock)){
                             down = false;
                         }else{
@@ -118,12 +124,12 @@ public class GameManager : MonoBehaviour{
                         
                     }
                     if(down){
-                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(posX, 1.5f, posZ2), new Quaternion(0, 180, 0, 1));
-                        checkExplosion(posX, posZ2);
+                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(bombX, 1.5f, bombZ2), new Quaternion(0, 180, 0, 1));
+                        checkExplosion(bombX, bombZ2);
                         Destroy(exp, 0.5f);
                     }
                     //RIGHT
-                    if(right && boxs[x].transform.position.x == posX1 && boxs[x].transform.position.z == posZ){
+                    if(right && boxs[x].transform.position.x == bombX1 && boxs[x].transform.position.z == bombZ){
                         if(boxs[x].TryGetComponent(out Wall rock)){
                             right = false;
                         }else{
@@ -133,12 +139,12 @@ public class GameManager : MonoBehaviour{
                         
                     }
                     if(right){
-                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(posX1, 1.5f, posZ), new Quaternion(0, 180, 0, 1));
-                        checkExplosion(posX1, posZ);
+                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(bombX1, 1.5f, bombZ), new Quaternion(0, 180, 0, 1));
+                        checkExplosion(bombX1, bombZ);
                         Destroy(exp, 0.5f);
                     }
                     //LEFT
-                    if(left && boxs[x].transform.position.x == posX2 && boxs[x].transform.position.z == posZ){
+                    if(left && boxs[x].transform.position.x == bombX2 && boxs[x].transform.position.z == bombZ){
                         if(boxs[x].TryGetComponent(out Wall rock)){
                             left = false;
                         }else{
@@ -147,8 +153,8 @@ public class GameManager : MonoBehaviour{
                         }
                     }
                     if(left){
-                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(posX2, 1.5f, posZ), new Quaternion(0, 180, 0, 1));
-                        checkExplosion(posX2, posZ);
+                        GameObject exp = (GameObject) Instantiate(fire, new Vector3(bombX2, 1.5f, bombZ), new Quaternion(0, 180, 0, 1));
+                        checkExplosion(bombX2, bombZ);
                         Destroy(exp, 0.5f);
                     }
                 }
@@ -156,8 +162,8 @@ public class GameManager : MonoBehaviour{
             aux++;
         }
         //Explosion central
-        GameObject exp2 = (GameObject) Instantiate(fire, new Vector3(posX, 1.5f, posZ), new Quaternion(0, 180, 0, 1));
-        checkExplosion(posX, posZ);
+        GameObject exp2 = (GameObject) Instantiate(fire, new Vector3(bombX, 1.5f, bombZ), new Quaternion(0, 180, 0, 1));
+        checkExplosion(bombX, bombZ);
         Destroy(exp2, 0.5f);
     }
 
@@ -184,13 +190,13 @@ public class GameManager : MonoBehaviour{
                 powerUps[1].SetActive(true);
             }
             if(powerUps[0] != null && powerUps[0].activeSelf){
-                if(playerX == 0 && playerZ == -2){ //Posicion de donde esta el power Up
+                if(playerX == 0 && playerZ == 2){ //Posicion de donde esta el power Up
                     distance++;
                     Destroy(powerUps[0]);
                 }
             }
             if(powerUps[1] != null && powerUps[1].activeSelf){
-                if(posX == 0 && posZ == 2){
+                if(bombX == 0 && bombZ == -2){
                     distance++;
                     Destroy(powerUps[1]);
                 }
@@ -199,7 +205,116 @@ public class GameManager : MonoBehaviour{
     }
 
     void RandomBomb(){
+        boxs = GameObject.FindGameObjectsWithTag("Box").ToList();
+        int num = Random.Range(0, 10);
+        int randX = Random.Range(-6, 6);
+        int randZ = Random.Range(-6, 6);
+        bool occuped = false;
         
+        if(num <= 1){
+            if(boxs != null){
+                for(int x = 0; x < boxs.Count && !occuped; x++){
+                    if(boxs[x].transform.position.x == randX && boxs[x].transform.position.z == randZ){
+                        occuped = true;
+                    }
+                }
+            }
+
+            if(!occuped){
+                random = true;
+                GameObject clone = (GameObject) Instantiate(bomb, new Vector3(randX, 1, randZ), new Quaternion(0, 180, 0, 1));
+                randomBombX = randX;
+                randomBombZ = randZ;
+                Invoke("explodeRandom", 1.5f);
+                Invoke("repeatRandom", 5f);
+                Destroy(clone, 1.5f);
+            }
+        }
+    }
+
+    void repeatRandom(){
+        random = false;
+    }
+
+    void explodeRandom(){
+        boxs = GameObject.FindGameObjectsWithTag("Box").ToList();
+        bool up    = true;
+        bool down  = true;
+        bool right = true;
+        bool left  = true;
+
+        if(play){
+            audios[1].Play();
+        }
+        
+        float randomBombZ1 = randomBombZ + 1;
+        float randomBombZ2 = randomBombZ - 1;
+        float randomBombX1 = randomBombX + 1;
+        float randomBombX2 = randomBombX - 1;
+
+        if(boxs != null){
+            for(int x = 0; x < boxs.Count; x++){
+                //UP
+                if(up && boxs[x].transform.position.x == randomBombX && boxs[x].transform.position.z == randomBombZ1){ //Puedo mirar arriba y hay algo ahí
+                    if(boxs[x].TryGetComponent(out Wall rock)){ //Si es roca dejo de mirar
+                        up = false;
+                    }else{ //Si no, es caja, la destruyo
+                        Destroy(boxs[x]);
+                    }
+                    
+                }
+                if(up){ //Si arriba no hay roca, creo la explosion y la compruebo
+                    GameObject exp = (GameObject) Instantiate(fire, new Vector3(randomBombX, 1.5f, randomBombZ1), new Quaternion(0, 180, 0, 1));
+                    checkExplosion(randomBombX, randomBombZ1);
+                    Destroy(exp, 0.5f);
+                }
+                //DOWN
+                if(down && boxs[x].transform.position.x == randomBombX && boxs[x].transform.position.z == randomBombZ2){
+                    if(boxs[x].TryGetComponent(out Wall rock)){
+                        down = false;
+                    }else{
+                        Destroy(boxs[x]);
+                    }
+                    
+                }
+                if(down){
+                    GameObject exp = (GameObject) Instantiate(fire, new Vector3(randomBombX, 1.5f, randomBombZ2), new Quaternion(0, 180, 0, 1));
+                    checkExplosion(randomBombX, randomBombZ2);
+                    Destroy(exp, 0.5f);
+                }
+                //RIGHT
+                if(right && boxs[x].transform.position.x == randomBombX1 && boxs[x].transform.position.z == randomBombZ){
+                    if(boxs[x].TryGetComponent(out Wall rock)){
+                        right = false;
+                    }else{
+                        Destroy(boxs[x]);
+                    }
+                    
+                }
+                if(right){
+                    GameObject exp = (GameObject) Instantiate(fire, new Vector3(randomBombX1, 1.5f, randomBombZ), new Quaternion(0, 180, 0, 1));
+                    checkExplosion(randomBombX1, randomBombZ);
+                    Destroy(exp, 0.5f);
+                }
+                //LEFT
+                if(left && boxs[x].transform.position.x == randomBombX2 && boxs[x].transform.position.z == randomBombZ){
+                    if(boxs[x].TryGetComponent(out Wall rock)){
+                        left = false;
+                    }else{
+                        Destroy(boxs[x]);
+                    }
+                }
+                if(left){
+                    GameObject exp = (GameObject) Instantiate(fire, new Vector3(randomBombX2, 1.5f, randomBombZ), new Quaternion(0, 180, 0, 1));
+                    checkExplosion(randomBombX2, randomBombZ);
+                    Destroy(exp, 0.5f);
+                }
+            }
+        }
+        //Explosion central
+        GameObject exp2 = (GameObject) Instantiate(fire, new Vector3(randomBombX, 1.5f, randomBombZ), new Quaternion(0, 180, 0, 1));
+        checkExplosion(randomBombX, randomBombZ);
+        Destroy(exp2, 0.5f);
     }
 
     void Pause(){
