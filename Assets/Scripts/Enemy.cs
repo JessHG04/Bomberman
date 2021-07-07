b2a81c648da489a9f919f7d354427c8e71914369
Jessica Hernández Gómez <jessicahg04@gmail.com>
2021-07-07 11:22:28 +0200

    Robot collider fixed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour{
-    
+    GameObject player;
+    private NavMeshAgent agentEnemy;
+    //private Animator anim;
     void Start(){
-        
+        agentEnemy = this.GetComponent<NavMeshAgent>();
+        //anim = this.GetComponent<Animator>();
+        player = GameObject.Find("Player(Clone)");
     }
 
     void Update(){
-        
+        agentEnemy.destination = player.transform.position;
+        //float distance = Vector3.Distance(agentEnemy.destination, agentPlayer.destination);
     }
 }
